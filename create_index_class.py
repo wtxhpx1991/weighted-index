54fed36f6cfb7cb1dab4d6d52a5e3491c358a210
wtxhpt1991 <wtxhpx1991@126.com>
2019-03-11 19:52:42 +0800

    haha

---
+++ create_index_class.py
@@ -0,0 +1,82 @@
+
+class Student(object):
+    # 实例的变量名如果以__开头，就变成了一个私有变量（private）
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def print_score(self):
+        print("%s's score is : %s" % (self.__name, self.__score))
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError("bad score")
+
+
+bart = Student("bart john", 123)
+bart.get_score()
+bart.print_score()
+bart.set_score(10)
+
+
+class Animal(object):
+    def run(self):
+        print("Animal is running")
+
+
+class Dog(Animal):
+    pass
+
+
+class Cat(Animal):
+    pass
+
+
+dog = Dog()
+dog.run()
+
+type(bart)
+type(dog)
+dir(bart)
+bart.__le__
+
+# 想要限制实例的属性怎么办
+
+
+class boy(object):
+    __slots__ = ('name', 'age')
+
+
+s = boy()
+s.name = 'john'
+s.age = 23
+s.score = 33
+
+
+# @property：将类中的一个方法变成属性调用
+
+class Student1(object):
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError("bad input")
+        if value < 0 or value > 100:
+            raise ValueError("score must between 0 and 100")
+        self._score = value
+
+
+s = Student1()
+s.score = 60
+s.score = 9999
--- get_data_from_wind.py
+++ get_data_from_wind.py
@@ -1,11 +1,7 @@
+import functools
+from operator import itemgetter
 import numpy as np
 import pandas as pd
-
-
-x = abs(-10)
-x
-f = abs
-f
 
 
 def addabs(x, y, f):
@@ -13,3 +9,59 @@ def addabs(x, y, f):
 
 
 addabs(-4, -5, abs)
+
+
+def f(x):
+    return x*x
+
+
+r = map(f, range(10))
+list(r)
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+sorted(L, key=itemgetter(0))
+sorted(L, key=lambda t: t[0])
+
+
+def lazysum(*args):
+    def getsum():
+        ax = 0
+        for i in args:
+            ax = ax+i
+        return ax
+    return getsum
+
+
+f = lazysum(1, 2, 3, 4, 5)
+f
+
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+f1()
+f2()
+f3()
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('call %s:' % func.__name__)
+        return func(*args, **kwargs)
+    return wrapper
+
+@log
+def now():
+    print('2018-02-03')
+now()
+now.__name__
+
+
